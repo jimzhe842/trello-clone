7aa781bf3e43b35328324fd60da052a89f8803c7
Kyle <kaledoux@gmail.com>
2021-06-15 13:15:53 -0400

    GET /boards/:id route and controller

--- server/controllers/boardsController.js
+++ server/controllers/boardsController.js
@@ -1,30 +1,40 @@
-const Board = require("../models/board");
-const HttpError = require("../models/httpError");
-const { validationResult } = require("express-validator");
+const Board = require('../models/board');
+const HttpError = require('../models/httpError');
+const { validationResult } = require('express-validator');
 
 const getBoards = (req, res, next) => {
-  Board.find({}, "title _id createdAt updatedAt")
-    .then(boards => {
-      res.json({
-        boards,
-      })
-    })
+	Board.find({}, 'title _id createdAt updatedAt').then((boards) => {
+		res.json({
+			boards
+		});
+	});
+};
+
+const getBoard = (req, res, next) => {
+	const id = req.params.id;
+
+	Board.findOne({ _id: id }, 'title _id createdAt updatedAt').populate('lists').exec((err, lists) => {
+		if (err) {
+			console.log(err);
+			next(new HttpError('boardId not found, please try again', 404));
+		}
+		res.json(lists);
+	});
 };
 
 const createBoard = (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    Board.create(req.body.board)
-      .then((board) => {
-        Board.find({ _id: board._id }, "title _id createdAt updatedAt").then(board => res.json({ board }))
-      })
-      .catch(err =>
-        next(new HttpError("Creating board failed, please try again", 500))
-      );
-  } else {
-    return next(new HttpError("The input field is empty.", 404));
-  }
+	const errors = validationResult(req);
+	if (errors.isEmpty()) {
+		Board.create(req.body.board)
+			.then((board) => {
+				Board.find({ _id: board._id }, 'title _id createdAt updatedAt').then((board) => res.json({ board }));
+			})
+			.catch((err) => next(new HttpError('Creating board failed, please try again', 500)));
+	} else {
+		return next(new HttpError('The input field is empty.', 404));
+	}
 };
 
 exports.getBoards = getBoards;
+exports.getBoard = getBoard;
 exports.createBoard = createBoard;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,12 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
-const boardsController = require("../controllers/boardsController");
-const { validateBoard } = require("../validators/validators");
+const boardsController = require('../controllers/boardsController');
+const { validateBoard } = require('../validators/validators');
 
+router.get('/boards', boardsController.getBoards);
 
-router.get('/boards',boardsController.getBoards );
+router.get('/boards/:id', boardsController.getBoard);
 
-router.post('/boards', validateBoard, boardsController.createBoard );
+router.post('/boards', validateBoard, boardsController.createBoard);
 
 module.exports = router;
