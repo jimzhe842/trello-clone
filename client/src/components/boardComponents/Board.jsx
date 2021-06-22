3d531629832e73516c697c56513b56d862db6d6d
Kyle Ledoux <kaledoux@gmail.com>
2021-06-22 14:01:36 -0400

    Board displaying under card modal

--- client/src/components/boardComponents/Board.jsx
+++ client/src/components/boardComponents/Board.jsx
@@ -1,14 +1,34 @@
 
 import React, {useEffect} from 'react';
-import { useDispatch, } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Header from './Header';
 import Lists from './Lists';
 import Sidebar from './Sidebar';
 import { getBoard } from "../../actions/BoardActions";
 
 const Board = (props) => {
-   const id = props.match.params.id;
-   const dispatch = useDispatch();
+	const path = props.location.pathname;
+	const cards = useSelector(state => state.cards)
+		console.log("cards in getBoard", cards);
+
+	const getBoardIdFromCard = () => {
+		const cardId = props.match.params.id
+		const currentlyDisplayedCard = cards.find((card) => card._id === cardId);
+		console.log(currentlyDisplayedCard);
+		return currentlyDisplayedCard.boardId
+	}
+
+	const findBoardId = () => {
+		if (path.match(/\/boards\//)) {
+			return props.match.params.id
+		} else {
+			return getBoardIdFromCard()
+		}
+	}
+
+  const id = findBoardId();
+  const dispatch = useDispatch();
+	console.log("props location", props);
 
   useEffect(() => {
    dispatch(getBoard(id))
