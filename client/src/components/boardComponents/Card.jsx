226b93e38ed2fe7e858e489c400aecea96727b4e
Elizabeth Tackett <emctackett@gmail.com>
2021-06-22 13:42:22 -0500

    remove comment

--- client/src/components/boardComponents/Card.jsx
+++ client/src/components/boardComponents/Card.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Card = ({ title, _id }) => {
-	console.log('card title', title);
 	return (
 		<Link to={`/cards/${_id}`}>
 			<div className="card-background">
